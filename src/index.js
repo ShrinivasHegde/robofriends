430e89d354c83cbf92cae39d10789aaddaeebb36
Shrinivas Hegde <mkshrinivashegde@gmail.com>
2023-04-20 12:25:36 +0530

    BugFix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './containers/App.js'
@@ -7,11 +7,19 @@ import 'tachyons'
 
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//React18 from
+//const root = ReactDOM.createRoot(document.getElementById('robo'));
+//til here react 18
+
 // root.render(
 //  <h1>Hello World</h1> );
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+//React 17 - ReactDOM.render(<App/>, document.getElementById('robo'))
+//React 18 from 
+ReactDOM.createRoot(document.getElementById('robo')).render(<App/>)
+//root.render(<App/>)
+//React 18 till here
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
